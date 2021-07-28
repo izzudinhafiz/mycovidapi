bbb1c49cd1f36b7d6b76310ed3b285d80a48fb01
Izzudin Hafiz <izzudin.hafiz@gmail.com>
2021-07-29 00:59:43 +0800

    Changes to links

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ api.add_resource(Clusters, "/api/v1/clusters")
 
 @app.route("/")
 def home_route():
-    return 'h1>Covid-19 Malaysia Data API V1. Created by <a href="https://izzudinhafiz.com">Izzudin Hafiz</a></h1>\
-            <p>Documentation on how to use is hosted on <a href=#>Postman</a></p>\
+    return '<h1>Covid-19 Malaysia Data API V1. Created by <a href="https://izzudinhafiz.com">Izzudin Hafiz</a></h1>\
+            <p>Documentation on how to use is hosted on <a href="https://documenter.getpostman.com/view/13724658/TzsbKn2w">Postman</a></p>\
             <p>This is open source project. For source code visit <a href="https://github.com/izzudinhafiz/mycovidapi">Github Repo</a></p>'
 
 
